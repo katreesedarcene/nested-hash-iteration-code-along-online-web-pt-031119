7d5566083a80f36d9d51df2ea10cfa3c56baabc2
Kat B <katreesebarnes@gmail.com>
2019-03-20 16:29:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,8 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |name, data|
-    data.each do |attribute, value|
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
